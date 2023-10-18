21cf829aa7502584db7fe6263d8c17e170d96f99
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2023-10-18 11:28:02 +0300

    feat: add an AppInput component

---
+++ src/components/AppInput.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const modelValue = defineModel<string>({ required: true })
+</script>
+
+<template>
+  <input
+    v-model="modelValue"
+    class="app-input"
+  >
+</template>
+
+<style scoped>
+.app-input {
+  padding: 0.5rem;
+  border-radius: 0.25rem;
+  border: 2px solid lightgrey;
+  outline: none;
+  transition: all 0.25s linear;
+}
+
+.app-input:focus {
+  border: 2px solid lightskyblue;
+}
+</style>
